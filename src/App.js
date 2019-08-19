41f9afc37e943e9a28a5db4720bd761de06bc1c6
José Martín Lopez <jmlopez@admins-MacBook-Pro-2.local>
2019-08-19 09:39:36 -0300

    bla

--- src/App.js
+++ src/App.js
@@ -3,23 +3,37 @@ import List from './components/listComponent';
 import Form from './components/formComponent';
 import Grid from "@material-ui/core/Grid";
 import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
+import {makeStyles} from "@material-ui/core";
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 function App() {
+    const classes = useStyles();
+
   return (
-      <Container>
-          <Grid item xs={6}>
+      <div className={classes.root}>
+      <Grid container spacing={3}>
+          <Grid item className={classes.paper} xs={4}>
               <Paper>
                 <Form/>
               </Paper>
           </Grid>
-          <Grid item xs={6}>
+          <Grid item className={classes.paper} xs={4}>
               <Paper>
                   <List/>
               </Paper>
           </Grid>
-      </Container>
+      </Grid>
+      </div>
   );
 }
 
